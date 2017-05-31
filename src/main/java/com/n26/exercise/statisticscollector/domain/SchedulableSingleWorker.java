94349c1b7a8da7538dfd95e02688c75405e028dc
Filippo Machi <filippo.machi@lastminute.com>
2017-05-31 22:02:44 +0200

    Moved common logic to an ancestor for schedulable task

--- src/main/java/com/n26/exercise/statisticscollector/domain/SchedulableSingleWorker.java
+++ src/main/java/com/n26/exercise/statisticscollector/domain/SchedulableSingleWorker.java
@@ -7,17 +7,20 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public abstract class SchedulableSingleThread
+public abstract class SchedulableSingleWorker
 {
-  private final static Logger logger = LoggerFactory.getLogger(SchedulableSingleThread.class);
+  private final static Logger logger = LoggerFactory.getLogger(SchedulableSingleWorker.class);
 
   private final ScheduledExecutorService scheduledExecutorService;
   private final long interval;
   private final TimeUnit period;
+  private final AtomicBoolean running = new AtomicBoolean(false);
+
   private Runnable worker;
 
-  public SchedulableSingleThread(ScheduledExecutorService scheduledExecutorService,
+  public SchedulableSingleWorker(ScheduledExecutorService scheduledExecutorService,
                                  long interval,
                                  TimeUnit period)
   {
@@ -29,20 +32,40 @@ public abstract class SchedulableSingleThread
   @PostConstruct
   public void init()
   {
+    running.set(true);
     this.worker = getWorker();
-    logger.info("Starting worker {}.",worker.getClass());
+    logger.info("Starting worker {} with delay {} period {} and time unit {}",
+                workerName(),
+                interval,
+                interval,
+                period);
     scheduledExecutorService.scheduleAtFixedRate(worker,
                                                  interval,
                                                  interval,
                                                  period);
   }
 
-  protected abstract Runnable getWorker();
-
   @PreDestroy
   public void destroy()
   {
-    logger.info("Stopping worker {}.",worker.getClass());
+    running.set(false);
+    logger.info("Stopping worker {}.", workerName());
     scheduledExecutorService.shutdownNow();
+  }
+
+  private String workerName()
+  {
+    if (worker != null)
+    {
+      return worker.getClass().getName();
+    }
+    return "NONE";
+  }
+
+  protected abstract Runnable getWorker();
+
+  public boolean isRunning()
+  {
+    return running.get();
   }
 }
