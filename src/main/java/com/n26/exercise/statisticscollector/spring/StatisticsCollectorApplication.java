5a207e555c386a970d3de6c66df6c63c5133bc8a
Filippo Machi <filippo.machi@lastminute.com>
2017-05-31 23:40:10 +0200

    Fixed package scan

--- src/main/java/com/n26/exercise/statisticscollector/spring/StatisticsCollectorApplication.java
+++ src/main/java/com/n26/exercise/statisticscollector/spring/StatisticsCollectorApplication.java
@@ -3,7 +3,7 @@ package com.n26.exercise.statisticscollector.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.n26.exercise.statisticscollector.spring","com.n26.exercise.statisticscollector.api.controllers"})
 public class StatisticsCollectorApplication
 {
 
